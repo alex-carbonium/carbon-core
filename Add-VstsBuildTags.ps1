f8471ba4554c526d877ad88317cdb79cf08718fb
Dennis Frostlander <dennis.frostlander@gmail.com>
2016-10-24 21:45:54 +0200

    build tags

---
+++ Add-VstsBuildTags.ps1
@@ -0,0 +1,4 @@
+﻿$base = $Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI + $Env:SYSTEM_TEAMPROJECT
+
+Invoke-RestMethod -Method Put -Uri "$base/_apis/build/builds/$Env:BUILD_BUILDID/tags/${Env:BUILD_SOURCEBRANCH}?api-version=2.0"`
+    -Headers @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }
