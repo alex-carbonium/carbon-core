f9bd6f882c0c47bdc45f14481b8599c9c8a3d21b
Alex <alex@carbonium.io>
2017-06-18 17:05:13 +0200

    fixed bug with converting polygons to symbols

--- mylibs/framework/arrangeStrategy/GroupArrangeStrategy.ts
+++ mylibs/framework/arrangeStrategy/GroupArrangeStrategy.ts
@@ -2,6 +2,7 @@ import ArrangeStrategy from "./ArrangeStrategy";
 import { ArrangeStrategies } from "../Defs";
 import Rect from "math/rect";
 import Point from "math/point";
+import Matrix from "math/matrix";
 import { IGroupContainer } from "carbon-core";
 
 var GroupArrangeStrategy = {
@@ -11,13 +12,13 @@ var GroupArrangeStrategy = {
             return;
         }
 
-        if (items.length === 1 && container.wrapSingleChild()) {
-            var props = items[0].selectLayoutProps(true);
-            props.m = container.parent().globalViewMatrixInverted().appended(props.m);
-            container.setProps(props, changeMode);
-            items[0].resetTransform();
-            return;
-        }
+        // if (items.length === 1 && container.wrapSingleChild()) {
+        //     var props = items[0].selectLayoutProps(true);
+        //     props.m = container.parent().globalViewMatrixInverted().appended(props.m);
+        //     container.setProps(props, changeMode);
+        //     items[0].resetTransform(changeMode);
+        //     return;
+        // }
 
         var angle = Math.round(items[0].getRotation());
         var sameAngle = !!angle;
@@ -61,31 +62,7 @@ var GroupArrangeStrategy = {
             var h = yMax - yMin;
             var br = null;
 
-            // if (sameAngle){
-            //     var center = new Point(xMin + w/2, yMin + h/2);
-            //     xMax = Number.NEGATIVE_INFINITY;
-            //     yMax = Number.NEGATIVE_INFINITY;
-            //     xMin = Number.POSITIVE_INFINITY;
-            //     yMin = Number.POSITIVE_INFINITY;
-            //
-            //     for (let i = 0, l = items.length; i < l; ++i) {
-            //         let child = items[i];
-            //         child.applyRotation(-angle, center);
-            //
-            //         let outerBox = child.getBoundingBox(true);
-            //         xMax = Math.max(xMax, outerBox.x + outerBox.width);
-            //         xMin = Math.min(xMin, outerBox.x);
-            //         yMax = Math.max(yMax, outerBox.y + outerBox.height);
-            //         yMin = Math.min(yMin, outerBox.y);
-            //     }
-            //
-            //     br = new Rect(xMin, yMin, xMax - xMin, yMax - yMin);
-            //
-            //     container.applyRotation(angle, center);
-            // }
-            // else{
             br = new Rect(xMin, yMin, w, h);
-            //}
 
             container.prepareAndSetProps({ br }, changeMode);
         }
